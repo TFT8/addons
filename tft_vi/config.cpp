ccfa74d7f49f3f88217137ae7fa4b5e7dc3069c2
Pawel W <pawel.wawron@gmail.com>
2016-06-14 21:35:12 +0200

    VI refactoring

--- tft_vi/config.cpp
+++ tft_vi/config.cpp
@@ -4,7 +4,7 @@
 		units[] = {};
 		weapons[] = {};
 		requiredVersion = 0.6;
-		requiredAddons[] = {"A3_3DEN", "cba_xeh"};
+		requiredAddons[] = {"cba_xeh","cba_xeh_a3"};
 		version = 2.0;
 		versionStr = 2.0;
 		versionAr[] = {2, 0};
@@ -12,8 +12,12 @@
 	};
 };
 
-class Extended_PostInit_EventHandlers {
-    class tft_vi {
-        killed = "[_this select 0, [profileNamespace, '[Current gear]']] call BIS_fnc_saveInventory";
+class Extended_Killed_EventHandlers {
+    class CAManBase {
+        class tft_vi {
+            killed = "if(isPlayer (_this select 0) && getMissionConfigValue ['respawnTemplates', []] isEqualTo ['MenuPosition']) then { \
+                        [_this select 0, [profileNamespace, '[Current gear]']] call BIS_fnc_saveInventory; \
+                      };";
+        };
     };
 };
