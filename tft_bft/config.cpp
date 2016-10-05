b4f9cf25107980720f238b63489f6681e7ef91be
Pawel W <theYoursTruly@users.noreply.github.com>
2016-10-05 17:16:42 +0200

    Moving callsign assignment from sandboxes (config)

--- tft_bft/config.cpp
+++ tft_bft/config.cpp
@@ -1,35 +1,30 @@
-﻿class CfgPatches {
-	class tft_bft
-    {
-		units[] = {};
-		weapons[] = {};
-		requiredVersion = 0.6;
-		requiredAddons[] = {"ace_common"};
-		version = 2.0;
-		versionStr = 2.0;
-		versionAr[] = {2, 0};
-		author = "YoursTruly";
-	};
+class CfgPatches {
+    class tft_bft {
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = 0.6;
+        requiredAddons[] = {"ace_common"};
+        version = 2.0;
+        versionStr = 2.0;
+        versionAr[] = {2, 0};
+        author = "YoursTruly";
+    };
 };
 
-#include "dialog.hpp"
+#include "UI\dialog.hpp"
+#include "UI\changeCallsignDialog.hpp"
 
 class CfgFunctions {
     class tft_bft {
-		class tft_bft {
-			file = "tft_bft\functions";
-			class initClient;
+        class tft_bft {
+            file = "tft_bft\functions";
+            class initClient { postInit=1; };
+            class assignCallsigns { preInit=1; };
             class addMapEH;
             class drawGroupIcon;
             class drawUnitIcon;
             class onDialogOpen;
             class onDialogClose;
-		};
-	};
-};
-
-class Extended_PostInit_EventHandlers {
-    class tft_bft {
-        init = "call tft_bft_fnc_initClient";
+        };
     };
 };
