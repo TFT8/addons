77b358eeb06d241495a2fa4e8dd9ac9cb2eb256e
Bryan Boru <bbsoapbox@gmail.com>
2016-11-05 12:18:03 -0400

    Added TFT Radios Configs
    Config.cpp and crates.hpp

---
+++ tft_tfar/crates.hpp
@@ -0,0 +1,31 @@
+class Box_NATO_Support_F;
+
+class TF_NATO_Radio_Crate: Box_NATO_Support_F {
+	author = "S4 @ TFT8";
+	displayName = "TFT8 Radio Crate";
+	class TransportItems {};
+	class TransportMagazines{};
+	class TransportWeapons{};
+	class TransportBackpacks {
+		class _xx_rt1523g_tft_ucp {
+			count = 5;
+			backpack = "rt1523g_tft_ucp";
+		};
+		class _xx_rt1523g_tft_ocp {
+			count = 5;
+			backpack = "rt1523g_tft_ocp";
+		};
+		class _xx_rt1523g_tft_ucp_big {
+			count = 5;
+			backpack = "rt1523g_tft_ucp_big";
+		};
+		class _xx_rt1523g_tft_ocp_big {
+			count = 5;
+			backpack = "rt1523g_tft_ocp_big";
+		};
+        class _xx_rt1523g_tft_wnt_big {
+			count = 5;
+			backpack = "rt1523g_tft_wnt_big";
+        };
+	};
+};
