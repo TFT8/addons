d8fc6c1f70fcdba0a8609fa3bda7ca0a63367c34
Bryan Boru <bbsoapbox@gmail.com>
2016-11-08 17:42:57 -0500

    Textures Update
    replaced dummy textures with completed patterns, updated readMe with
    additional classnames
    updated config with better display names

--- tft_tfar/config.cpp
+++ tft_tfar/config.cpp
@@ -1,3 +1,4 @@
+#include "BIS_AddonInfo.hpp"
 class CfgPatches {
     class tft_tfar {
         units[] =  {};
@@ -26,25 +27,25 @@ class CfgVehicles {
 
     class tf_rt1523g_ucp: tf_rt1523g {
         author = "S4 @ TFT8 / Fitzgerald, Truly";
-        displayName = "UCP RT-1523G (ASIP)";
+        displayName = "RT-1523G (ASIP) TFT8 [UCP]";
         descriptionShort = "RT-1523G (ASIP) UCP long range radio 20km";
         hiddenSelections[] = {"camo"};
         hiddenSelectionsTextures[] = {"\tft_tfar\data\ucp.paa"};
     };
     class tf_rt1523g_ocp: tf_rt1523g_ucp {
-        displayName = "OCP RT-1523G (ASIP)";
+        displayName = "RT-1523G (ASIP) TFT8 [OCP]";
         descriptionShort = "RT-1523G (ASIP) OCP long range radio 20km";
         hiddenSelections[] = {"camo"};
         hiddenSelectionsTextures[] = {"\tft_tfar\data\ocp.paa"};
     };
     class tf_rt1523g_wnt: tf_rt1523g_ucp {
-        displayName = "WNT RT-1523G (ASIP)";
+        displayName = "RT-1523G (ASIP) TFT8 [WNT]";
         descriptionShort = "RT-1523G (ASIP) WNT long range radio 20km";
         hiddenSelections[] = {"camo"};
         hiddenSelectionsTextures[] = {"\tft_tfar\data\wnt.paa"};
     };
     class tf_rt1523g_ucp_big: tf_rt1523g_ucp {
-        displayName = "UCP RT-1523G (ASIP) Big";
+        displayName = "RT-1523G (ASIP) Big TFT8 [UCP]";
         descriptionShort = "RT-1523G (ASIP) UCP (Big) long range radio 20km";
         maximumLoad = 160;
         mass = 160;
@@ -53,7 +54,7 @@ class CfgVehicles {
         model="\task_force_radio_items\models\clf_nicecomm2";
     };
     class tf_rt1523g_ocp_big: tf_rt1523g_ucp {
-        displayName = "OCP RT-1523G (ASIP) Big";
+        displayName = "RT-1523G (ASIP) Big TFT8 [OCP]";
         descriptionShort = "RT-1523G (ASIP) OCP (Big) long range radio 20km";
         maximumLoad = 160;
         mass = 160;
@@ -62,7 +63,7 @@ class CfgVehicles {
         model="\task_force_radio_items\models\clf_nicecomm2";
     };
     class tf_rt1523g_wnt_big: tf_rt1523g_ucp {
-        displayName = "WNT RT-1523G (ASIP) Big";
+        displayName = "RT-1523G (ASIP) Big TFT8 [WNT]";
         descriptionShort = "RT-1523G (ASIP) WNT (Big) long range radio 20km";
         maximumLoad = 160;
         mass = 160;
