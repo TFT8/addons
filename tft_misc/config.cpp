4f895e817b33b45b7429991e128dcbda3a9ad230
Pawel W <pawel.wawron@gmail.com>
2017-02-02 19:59:00 +0100

    [tft_misc] AI uses RPG7 and RPG32 on Air and infantry now

--- tft_misc/config.cpp
+++ tft_misc/config.cpp
@@ -3,7 +3,7 @@ class CfgPatches {
         units[] = {};
         weapons[] = {};
         requiredVersion = 0.6;
-        requiredAddons[] = {"cba_xeh","cba_xeh_a3"};
+        requiredAddons[] = {"A3_Weapons_F","A3_Sounds_F","A3_Weapons_F_Exp","cba_xeh","cba_xeh_a3"};
         version = 2.0;
         versionStr = 2.0;
         versionAr[] = {2, 0};
@@ -33,5 +33,20 @@ class CfgFunctions {
             class draw3D;
             class assignTarget;
         };
+    };
+};
+
+class CfgAmmo {
+    class RocketBase;   //External Reference
+
+    class R_PG7_F : RocketBase {
+        aiAmmoUsageFlags = "64 + 128 + 256 + 512";
+        allowAgainstInfantry = 1;
+        cost = 100;
+    };
+    class R_PG32V_F : RocketBase {
+        aiAmmoUsageFlags = "64 + 128 + 256 + 512";
+        allowAgainstInfantry = 1;
+        cost = 200;
     };
 };
