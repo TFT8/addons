f5ff35a5f2432d8b3f8ade665bfbc7683a080f3d
ampersand38 <ampersand38@gmail.com>
2020-04-29 23:33:14 -0400

    Server init and opslog

---
+++ tft_server/config.cpp
@@ -0,0 +1,18 @@
+class CfgPatches {
+    class tft_server {
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = 0.6;
+        requiredAddons[] = {"cba_xeh","cba_xeh_a3"};
+        version = 2.0;
+        versionStr = 2.0;
+        versionAr[] = {2, 0};
+        author = "Ampersand";
+    };
+};
+
+class Extended_PreInit_EventHandlers {
+    class tft_server {
+        init = "call compile preprocessFileLineNumbers 'tft_init.sqf'";
+    };
+};
