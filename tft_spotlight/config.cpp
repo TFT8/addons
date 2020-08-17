e152c9439a5900a8fcaa871894945e7fcbccbf5d
ampersand38 <ampersand38@gmail.com>
2020-08-17 17:14:35 -0400

    Revert "new config path"
    This reverts commit 6bfd19833c0aa7916e16ba76664cde47d41b5b2c.

--- tft_spotlight/config.cpp
+++ tft_spotlight/config.cpp
@@ -11,17 +11,22 @@ class CfgPatches {
     };
 };
 
-class CfgMainMenuSpotlight
+class RscStandardDisplay;
+class RscDisplayMain: RscStandardDisplay
 {
-    class TFTServer
-    {
-        text = "TFT8 Private Server"; // Text displayed on the square button, converted to upper-case
-        textIsQuote = 0; // 1 to add quotation marks around the text
-        picture = "\tft_spotlight\logo_tft8_512.paa"; // Square picture, ideally 512x512
-        //video = "\a3\Ui_f\Video\spotlight_1_Apex.ogv"; // Video played on mouse hover
-        //action = "0 = [_this, '127.0.0.1', '2302', ''] execVM '\x\joinServer.sqf';";
-        action = "0 = [_this, '89.163.140.110', '2322', 'yourpasshere'] execVM '\tft_spotlight\joinServer.sqf';";
-        actionText = "Join the TFT8 Private Server"; // Text displayed in top left corner of on-hover white frame
-        condition = "true"; // Condition for showing the spotlight
-    };
+	class Spotlight
+	{
+		//delete;
+		class TFTServer
+		{
+			text = "TFT8 Private Server"; // Text displayed on the square button, converted to upper-case
+			textIsQuote = 0; // 1 to add quotation marks around the text
+			picture = "\tft_spotlight\logo_tft8_512.paa"; // Square picture, ideally 512x512
+			//video = "\a3\Ui_f\Video\spotlight_1_Apex.ogv"; // Video played on mouse hover
+			//action = "0 = [_this, '127.0.0.1', '2302', ''] execVM '\x\joinServer.sqf';";
+			action = "0 = [_this, '89.163.140.110', '2322', 'yourpasshere'] execVM '\tft_spotlight\joinServer.sqf';";
+			actionText = "Join the TFT8 Private Server"; // Text displayed in top left corner of on-hover white frame
+			condition = "true"; // Condition for showing the spotlight
+		};
+	};
 };
