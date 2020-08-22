2fae014d428d6716395eaf79bb7735e7a77a7438
ampersand38 <ampersand38@gmail.com>
2020-08-21 22:58:40 -0400

    Use Macros to make new versions simpler

--- tft_helmets/config.cpp
+++ tft_helmets/config.cpp
@@ -1,173 +1,55 @@
-class CfgPatches
-{
-	class tft8_helmets
-	{
-		units[]={};
-		weapons[]={""};
-		requiredAddons[]={"rhsusf_infantry2"};
-		version="1.0.1";
-		author[]={"Shuff13","Mik92"};
-		authorUrl="https://tft8.com/";
+class CfgPatches {
+	class tft_helmets {
+		units[] = {};
+		weapons[] = {""};
+		requiredAddons[] = {"rhsusf_infantry2"};
+		version = "1.0.1";
+		author[] = {"Shuff13", "Mik92"};
+		authorUrl = "https://tft8.com/";
 	};
 };
 
-class cfgWeapons
-{
+#define CONCAT(arg1, arg2, arg3) arg1##arg2##arg3
+
+#define QUOTE(x) #x
+
+#define TEXTURE(NAME) hiddenSelectionsTextures[] = { \
+		QUOTE(CONCAT(\tft_helmets\data\,NAME,_camo_co.paa)), \
+		QUOTE(CONCAT(\tft_helmets\data\,NAME,_patches_co.paa)), \
+};
+
+#define HGU56P(NAME) class tft8_hgu56p_visor_mask_##NAME : rhsusf_hgu56p_visor_mask_black { \
+    scope = 1; \
+    displayName = QUOTE([TFT8]HGU-56/P[NAME] (Visor/Mask)); \
+    picture = "\tft_helmets\ui\rhs_hgu56_visor_mask_blk_ca.paa"; \
+    TEXTURE(NAME) \
+}; \
+class tft8_hgu56p_mask_##NAME : rhsusf_hgu56p_mask_black { \
+    scope = 2; \
+    displayName = QUOTE([TFT8]HGU-56/P[NAME] (Mask)); \
+    picture = "\tft_helmets\ui\rhs_hgu56_visor_mask_blk_ca.paa"; \
+    TEXTURE(NAME) \
+}; \
+class tft8_hgu56p_visor_##NAME : rhsusf_hgu56p_visor_black { \
+    scope = 1; \
+    displayName = QUOTE([TFT8]HGU-56/P[NAME] (Visor)); \
+    picture = "\tft_helmets\ui\rhs_hgu56_visor_mask_blk_ca.paa"; \
+		TEXTURE(NAME) \
+}; \
+class tft8_hgu56p_##NAME : rhsusf_hgu56p_black { \
+    scope = 2; \
+    displayName = QUOTE([TFT8]HGU-56/P[NAME]); \
+    picture = "\tft_helmets\ui\rhs_hgu56_visor_mask_blk_ca.paa"; \
+    TEXTURE(NAME) \
+};
+
+class cfgWeapons {
 	class rhsusf_hgu56p_visor_mask_black;
 	class rhsusf_hgu56p_mask_black;
 	class rhsusf_hgu56p_visor_black;
 	class rhsusf_hgu56p_black;
-	
-	
-	class tft8_hgu56p_visor_mask_smoke: rhsusf_hgu56p_visor_mask_black
-    { 
-        scope = 2; 
-        displayName = "[TFT8] HGU-56/P (Smoke/Visor/Mask)"; 
-		author="Shuffl3";
-        picture = "\tft_helmets\ui\rhs_hgu56_visor_mask_blk_ca.paa"; //Thanks RHS TEAM!
-		hiddenSelections[] = {"camo","patches"};
-		hiddenSelectionsTextures[] =
-		{
-			"\tft_helmets\data\smoke_camo_co.paa",
-			"\tft_helmets\data\smoke_patches_co.paa"
-		};
-		hiddenSelectionsMaterials[] =
-		{
-			"\rhsusf\addons\rhsusf_infantry2\gear\head\hgu56\Data\rhs_hgu56.rvmat",
-			"\rhsusf\addons\rhsusf_infantry2\gear\head\hgu56\Data\rhs_hgu56_patches.rvmat"
-		};
-	};
-	
-	class tft8_hgu56p_mask_smoke: rhsusf_hgu56p_mask_black
-    { 
-        scope = 2; 
-        displayName = "[TFT8] HGU-56/P (Smoke/Mask)"; 
-		author="Shuffl3";
-        picture = "\tft_helmets\ui\rhs_hgu56_visor_mask_blk_ca.paa"; //Thanks RHS TEAM!
-		hiddenSelections[] = {"camo","patches"};
-		hiddenSelectionsTextures[] =
-		{
-			"\tft_helmets\data\smoke_camo_co.paa",
-			"\tft_helmets\data\smoke_patches_co.paa"
-		};
-		hiddenSelectionsMaterials[] =
-		{
-			"\rhsusf\addons\rhsusf_infantry2\gear\head\hgu56\Data\rhs_hgu56.rvmat",
-			"\rhsusf\addons\rhsusf_infantry2\gear\head\hgu56\Data\rhs_hgu56_patches.rvmat"
-		};
-	};
-	
-	class tft8_hgu56p_visor_smoke: rhsusf_hgu56p_visor_black
-    { 
-        scope = 2; 
-        displayName = "[TFT8] HGU-56/P (Smoke/Visor)"; 
-		author="Shuffl3";
-        picture = "\tft_helmets\ui\rhs_hgu56_visor_mask_blk_ca.paa"; //Thanks RHS TEAM!
-		hiddenSelections[] = {"camo","patches"};
-		hiddenSelectionsTextures[] =
-		{
-			"\tft_helmets\data\smoke_camo_co.paa",
-			"\tft_helmets\data\smoke_patches_co.paa"
-		};
-		hiddenSelectionsMaterials[] =
-		{
-			"\rhsusf\addons\rhsusf_infantry2\gear\head\hgu56\Data\rhs_hgu56.rvmat",
-			"\rhsusf\addons\rhsusf_infantry2\gear\head\hgu56\Data\rhs_hgu56_patches.rvmat"
-		};
-	};
-	
-	class tft8_hgu56p_smoke: rhsusf_hgu56p_black
-    { 
-        scope = 2; 
-        displayName = "[TFT8] HGU-56/P (Smoke)"; 
-		author="Shuffl3";
-        picture = "\tft_helmets\ui\rhs_hgu56_visor_mask_blk_ca.paa"; //Thanks RHS TEAM!
-		hiddenSelections[] = {"camo","patches"};
-		hiddenSelectionsTextures[] =
-		{
-			"\tft_helmets\data\smoke_camo_co.paa",
-			"\tft_helmets\data\smoke_patches_co.paa"
-		};
-		hiddenSelectionsMaterials[] =
-		{
-			"\rhsusf\addons\rhsusf_infantry2\gear\head\hgu56\Data\rhs_hgu56.rvmat",
-			"\rhsusf\addons\rhsusf_infantry2\gear\head\hgu56\Data\rhs_hgu56_patches.rvmat"
-		};
-	};
-	
-    class tft8_hgu56p_visor_mask_swiss: rhsusf_hgu56p_visor_mask_black
-    { 
-        scope = 2; 
-        displayName = "[TFT8] HGU-56/P (Swiss/Visor/Mask)"; 
-		author="Mik92";
-        picture = "\tft_helmets\ui\rhs_hgu56_visor_mask_blk_ca.paa"; //Thanks RHS TEAM!
-		hiddenSelections[] = {"camo","patches"};
-		hiddenSelectionsTextures[] =
-		{
-			"\tft_helmets\data\swiss_camo_co.paa",
-			"\tft_helmets\data\swiss_patches_co.paa"
-		};
-		hiddenSelectionsMaterials[] =
-		{
-			"\rhsusf\addons\rhsusf_infantry2\gear\head\hgu56\Data\rhs_hgu56.rvmat",
-			"\rhsusf\addons\rhsusf_infantry2\gear\head\hgu56\Data\rhs_hgu56_patches.rvmat"
-		};
-	};
-	
-	class tft8_hgu56p_mask_swiss: rhsusf_hgu56p_mask_black
-    { 
-        scope = 2; 
-        displayName = "[TFT8] HGU-56/P (Swiss/Mask)"; 
-		author="Mik92";
-        picture = "\tft_helmets\ui\rhs_hgu56_visor_mask_blk_ca.paa"; //Thanks RHS TEAM!
-		hiddenSelections[] = {"camo","patches"};
-		hiddenSelectionsTextures[] =
-		{
-			"\tft_helmets\data\swiss_camo_co.paa",
-			"\tft_helmets\data\swiss_patches_co.paa"
-		};
-		hiddenSelectionsMaterials[] =
-		{
-			"\rhsusf\addons\rhsusf_infantry2\gear\head\hgu56\Data\rhs_hgu56.rvmat",
-			"\rhsusf\addons\rhsusf_infantry2\gear\head\hgu56\Data\rhs_hgu56_patches.rvmat"
-		};
-	};
-	
-	class tft8_hgu56p_visor_swiss: rhsusf_hgu56p_visor_black
-    { 
-        scope = 2; 
-        displayName = "[TFT8] HGU-56/P (Swiss/Visor)"; 
-		author="Mik92";
-        picture = "\tft_helmets\ui\rhs_hgu56_visor_mask_blk_ca.paa"; //Thanks RHS TEAM!
-		hiddenSelections[] = {"camo","patches"};
-		hiddenSelectionsTextures[] =
-		{
-			"\tft_helmets\data\swiss_camo_co.paa",
-			"\tft_helmets\data\swiss_patches_co.paa"
-		};
-		hiddenSelectionsMaterials[] =
-		{
-			"\rhsusf\addons\rhsusf_infantry2\gear\head\hgu56\Data\rhs_hgu56.rvmat",
-			"\rhsusf\addons\rhsusf_infantry2\gear\head\hgu56\Data\rhs_hgu56_patches.rvmat"
-		};
-	};
-	
-	class tft8_hgu56p_swiss: rhsusf_hgu56p_black
-    { 
-        scope = 2; 
-        displayName = "[TFT8] HGU-56/P (Swiss)"; 
-		author="Mik92";
-        picture = "\tft_helmets\ui\rhs_hgu56_visor_mask_blk_ca.paa"; //Thanks RHS TEAM!
-		hiddenSelections[] = {"camo","patches"};
-		hiddenSelectionsTextures[] =
-		{
-			"\tft_helmets\data\swiss_camo_co.paa",
-			"\tft_helmets\data\swiss_patches_co.paa"
-		};
-		hiddenSelectionsMaterials[] =
-		{
-			"\rhsusf\addons\rhsusf_infantry2\gear\head\hgu56\Data\rhs_hgu56.rvmat",
-			"\rhsusf\addons\rhsusf_infantry2\gear\head\hgu56\Data\rhs_hgu56_patches.rvmat"
-		};
-	};
+
+	HGU56P(Smoke)
+	HGU56P(Swiss)
+
 };
