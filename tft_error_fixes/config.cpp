927413b2a9676b516c46ac82f50dcaea18658fb7
ampersand38 <ampersand38@gmail.com>
2021-01-17 20:45:58 -0500

    Block RHS M123x errors

--- tft_error_fixes/config.cpp
+++ tft_error_fixes/config.cpp
@@ -12,6 +12,21 @@ class CfgVehicles {
     class Land_CraneRail_01_F;
     class Land_A_crane_rails: Land_CraneRail_01_F {};
     class Land_Misc_Cargo1a: Land_CraneRail_01_F {};
+    
+    class rhsusf_M1238A1_socom_d;
+    class rhsusf_M1238A1_socom_w: rhsusf_M1238A1_socom_d {};
+    class rhsusf_M1238A1_M2_socom_d;
+    class rhsusf_M1238A1_M2_socom_w: rhsusf_M1238A1_M2_socom_d {};
+    class rhsusf_M1238A1_Mk19_socom_d;
+    class rhsusf_M1238A1_Mk19_socom_w: rhsusf_M1238A1_Mk19_socom_d {};
+    class rhsusf_M1239_socom_d;
+    class rhsusf_M1239_socom_w: rhsusf_M1239_socom_d {};
+    class rhsusf_M1239_M2_Deploy_socom_d;
+    class rhsusf_M1239_M2_Deploy_socom_w: rhsusf_M1239_M2_Deploy_socom_d {};
+    class rhsusf_M1239_Mk19_Deploy_socom_d;
+    class rhsusf_M1239_Mk19_Deploy_socom_w: rhsusf_M1239_Mk19_Deploy_socom_d {};
+    class rhsusf_stryker_m1126_m2_d;
+    class rhsusf_stryker_m1126_m2_w: rhsusf_stryker_m1126_m2_d {};
 };
 
 class CfgWeapons {
